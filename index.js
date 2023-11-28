d36e4161dbf565c5f8908f13da0f4962252e1067
Alejandro-Suaza <138082745+Alejandro-Suaza@users.noreply.github.com>
2023-11-28 18:16:09 -0500

    Solicion Error

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const jsonServer = require("json-server"); 
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; 
+const jsonServer = require("json-server");
+const path = require("path");
 
+const server = jsonServer.create();
+const router = jsonServer.router(path.join("db.json")); 
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
+
+server.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://alejandro-suaza.github.io"); 
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Servidor JSON-Server en ejecución en el puerto ${port}`);
+});
