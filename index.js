b850c3d452e253132199a7538e7d1b140c09d55a
Alejandro-Suaza <138082745+Alejandro-Suaza@users.noreply.github.com>
2023-11-28 22:40:40 -0500

    Config Render

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const jsonServer = require("json-server"); 
+import jsonServer from "json-server";
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Servidor JSON-Server en ejecución en el puerto ${port}`);
+});
